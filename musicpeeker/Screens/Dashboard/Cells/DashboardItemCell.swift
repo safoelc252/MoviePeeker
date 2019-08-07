6838ce1941ae70cd7f2a289dcf6872a56cdcfba6
Cleofas Villarin <safoelc252@gmail.com>
2019-08-07 21:36:37 +0800

    add icons

---
+++ musicpeeker/Screens/Dashboard/Cells/DashboardItemCell.swift
@@ -0,0 +1,21 @@
+//
+//  DashboardItemCell.swift
+//  musicpeeker
+//
+//  Created by Cleofas Villarin on 07/08/2019.
+//  Copyright © 2019 appetiserco. All rights reserved.
+//
+
+import UIKit
+
+class DashboardItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
